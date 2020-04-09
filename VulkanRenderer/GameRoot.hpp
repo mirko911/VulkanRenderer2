09826467dd86842c9f8c22034c28e579b43d6adf
Mirko Rosenthal <mirko91194@googlemail.com>
2020-04-09 14:18:01 +0200

    Adds HandlerTransformation to GameRoot

--- VulkanRenderer/GameRoot.hpp
+++ VulkanRenderer/GameRoot.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include "Handler/HandlerGeometry.hpp"
+#include "Handler/HandlerTransformation.hpp"
 
 class GameRoot {
 public:
 	HandlerGeometry hGeometry;
+	HandlerTransformation hTransformation;
 };
