9a15e3c3052c513685afa178bd3e8ef12937f44a
Mirko Rosenthal <mirko91194@googlemail.com>
2020-04-21 15:59:59 +0200

    Adds linux build file

---
+++ makeLinux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+INCLUDE_LIBS="-I ../../Libs/glm -I ../../Libs/loguru -I ../../Libs/stb -I ../../Libs/tinyobjloader -I ../../Libs/glfw-3.3.2/include"
+LIB_FILES="-L Libs/loguru/ -lloguru -lvulkan -lglfw3 -lpthread -lm -ldl -lX11 -lstdc++"
+BUILD_OPTIONS="-std=c++17" #-Wall
+
+echo "Compile Shaders"
+cd VulkanRenderer/shaders/
+./compile.sh
+
+echo "Compiling C++ Code"
+
+cd ../x64
+
+g++ $BUILD_OPTIONS $INCLUDE_LIBS -c ../*.cpp
+g++ $BUILD_OPTIONS $INCLUDE_LIBS -c ../demos/*.cpp
+g++ $BUILD_OPTIONS $INCLUDE_LIBS -c ../Handler/*.cpp
+g++ $BUILD_OPTIONS $INCLUDE_LIBS -c ../input/*.cpp
+g++ $BUILD_OPTIONS $INCLUDE_LIBS -c ../Modules/*.cpp
+
+cd ../../
+
+echo "Linking"
+
+gcc -o VulkanRenderer/vulkanrenderer_x64 VulkanRenderer/x64/*.o $LIB_FILES
